9ffd96f5af4db201e9683cd00ebc38867fea202e
Athos Schrapett <athosschrapett@outlook.com>
2022-01-07 21:40:15 -0300

    Criação Componente de Cadastro de Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CadastrosClientesComponent } from './cadastros-clientes/cadastros-clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CadastrosClientesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cadastros-clientes/cadastros-clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastros',
+  templateUrl: './cadastros-clientes.component.html',
+  styleUrls: ['./cadastros-clientes.component.css']
+})
+export class CadastrosClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
